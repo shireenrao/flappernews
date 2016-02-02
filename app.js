74b49973f568e98daf09978fbf80576bb0e61d5f
Srinivas Nyayapati <shireenrao@gmail.com>
2016-02-02 12:09:13 -0500

    add upvotes

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ app.controller('MainCtrl', [
 function($scope){
   $scope.test = 'Hello world!';
   $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
   ];
+
+  $scope.addPost = function(){
+    if (!$scope.title || $scope.title === '') {return;}
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
+
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  };
 }]);
