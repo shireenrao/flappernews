18e3288316bf67dab739a560f869ab911ae846b1
Srinivas Nyayapati <shireenrao@gmail.com>
2016-02-02 12:17:54 -0500

    Add Service factory

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]
+  };
+  return o;
+}]);
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  
+  $scope.posts = posts.posts;
 
   $scope.addPost = function(){
     if (!$scope.title || $scope.title === '') {return;}
